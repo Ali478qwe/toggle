fe6f6a7515a97892f025938092731894b41c865b
Ali478qwe <a667908@gmail.com>
2025-05-09 23:45:28 +0430

    new update

--- toggle.js
+++ toggle.js
@@ -25,18 +25,3 @@
 function button(id,func){
       document.getElementById(id).addEventListener("click",func);
 }
-
-
-
-
-
-
-// function reload(isOn,text,color,is_State=false) {
-//       button.addEventListener("click", () => {is_State = !is_State;});
-//       isOn = is_State ?  text[0] : text[1];
-//       bcolor.style.backgroundColor = isOn ? color[0]:color[1];
-//       button.textContent = isOn ? text[0] : text[1];
-//       button.style.transform = isOn ? "translateX(100%)":"translateX(0%)";
-//       sendMessage(isOn ? text[0] : text[1]);
-//       reload(isOn);
-//     }
